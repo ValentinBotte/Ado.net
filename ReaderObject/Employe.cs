ec957ddd3d9b57ca165945562db587e2306afb28
BOTTE Valentin <botte.v@sio.lan>
2016-10-05 12:59:45 +0200

    init

---
+++ ReaderObject/Employe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderObject
+{
+    class Employe
+    {
+        public Int16 Numemp { get; set; }
+        public String Nomemp { get; set; }
+        public String Prenomemp { get; set; }
+        public String Poste { get; set; }
+        public Single Salaire { get; set; }
+        public Single? Prime { get; set; } // nullable
+        public String CodeProjet { get; set; }
+        public Int16? Superieur { get; set; } // nullable
+        /*
+        public Employe(short numemp, string nomemp, string prenomemp, string poste, float salaire, float? prime, string codeProjet, short? superieur)
+        {
+            Numemp = numemp;
+            Nomemp = nomemp;
+            Prenomemp = prenomemp;
+            Poste = poste;
+            Salaire = salaire;
+            Prime = prime;
+            CodeProjet = codeProjet;
+            Superieur = superieur;
+        }
+        */
+        public Employe(short numemp, string nomemp, string prenomemp, string poste, float salaire, float? prime)
+        {
+            Numemp = numemp;
+            Nomemp = nomemp;
+            Prenomemp = prenomemp;
+            Poste = poste;
+            Salaire = salaire;
+            Prime = prime;
+        }
+
+        public override string ToString()
+        {
+            return Numemp + " . " + Nomemp + " . " + Prenomemp + " . " + Poste + " . " + Salaire + " . " + Prime + " . " + CodeProjet + " . " + Superieur;
+        }
+    }
+
+}
---
+++ ReaderObject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ReaderObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                String oracledb = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.10.2.10)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=SLAM)));User Id = raimonsql; Password = sio; ";
+
+                String connectionString = oracledb;
+                OracleConnection connection = new OracleConnection();
+                connection.ConnectionString = oracledb;
+                connection.Open();
+
+                List<Employe> LesEmployes = new List<Employe>();
+                OracleCommand command = new OracleCommand("SELECT * FROM EMPLOYE",connection);
+                OracleDataReader reader;
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+      
+                    LesEmployes.Add(new Employe(Convert.ToInt16(reader["NUMEMP"]),
+                        reader["NOMEMP"] as String,
+                        reader["PRENOMEMP"] as String,
+                        reader["POSTE"] as String ,
+                        Convert.ToSingle(reader["SALAIRE"]),
+                        reader["Prime"] == DBNull.Value ?  Convert.ToSingle(reader["SALAIRE"]) : 0
+                        ));
+    
+                }
+                foreach (var employe in LesEmployes)
+                {
+                    Console.WriteLine(employe.ToString());
+                }
+
+                reader.Close();
+                connection.Close();
+                Console.ReadKey();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
